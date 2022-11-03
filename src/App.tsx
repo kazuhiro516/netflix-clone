6e6df9651025ff40b19af24fe608618e4a8a99ed
kurokawakazuhiro <k.kurokawa11@gmail.com>
2022-11-03 12:39:11 +0900

    バナーを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
-import { Row } from 'components/Row'
-import { requests } from './request.js'
+import { Row } from './components/Row'
+import { Banner } from './components/Banner'
+import { requests } from './request'
 
 function App() {
   return (
     <div className="App">
+      <Banner />
       <Row
         title="NETFLIX ORIGUINALS"
         fetchUrl={requests.feachNetflixOriginals}
